34945edc67680da7af30814a6bdd2d9de5768a45
tommy-hku19 <47239468+tommy-hku19@users.noreply.github.com>
2019-04-27 00:58:24 +0800

    Update update.cpp
    Finished the template of this program, will do the debugging in the next commit

--- update.cpp
+++ update.cpp
@@ -1,8 +1,12 @@
 #include "main.h"
 #include "update.h"
+#include "search.h"
+#include "inpututil.h"
 #include <string>
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
 using namespace main_header;
 using namespace std;
 
@@ -14,18 +18,6 @@ using namespace std;
  */
 /* For editing multiple entries, the subroutine replacec() should be called more than once. */
 
-// ------------------------------------------------
-// Function update_one: changes content of ONE particular
-// inventory entry. This function should implement the above mentioned algorithm,
-// but the details are to be input by the user. Perform any necessary
-// checks on user input.
-// @params vector<Commodity> &list: the inventory to modify
-// @return none
-// -----------------------------------------------
-void update_one(vector<Commodity> &list){
-	
-}
-
 // -------------------------------------
 // Function update: will be called immediately when
 // the user wants to update the inventory. Lead him to the right functions
@@ -35,20 +27,162 @@ void update_one(vector<Commodity> &list){
 // @return none
 // -------------------------------------
 void update(vector<Commodity> &list){
+	bool exit = 0;
+	string buffer;
+	bool input_ok = 0;
+	int criterion = 0;
 	
-}
-
-// ----------------------------------------
-// Function replacec: replaces the index-th item of the specified list (list[index])
-// with the item (target), therefore succeeding in amending contents of an entry.
-// Suitable input checks should be performed.
-// @params vector<Commodity> &list: the inventory to modify
-//		   int index: location of the object
-// 		   Commodity target: the object to replace to
-// @return none
-// ----------------------------------------
-void replacec(vector<Commodity> &list, int index, Commodity target){
-	
+	cout << "How would you like to update the commodity?\n";
+	cout << "1. one-by-one manually\n";
+	cout << "2. go back\n";
+	cout << "Please enter a number: ";
+	char input;
+	cin >> input;
+	if (input == '1'){
+		Commodity com = getCommodityData();
+		add_one(list, com);
+	}
+	else if (input == '2'){
+		return;
+	}
+	else{
+		while (not exit){
+			int index;
+			while (not input ok){
+				cout << "What would you like to do?\n";
+				cout << "1.Enter an id\n";
+				cout << "2.Type -1 to exit\n";
+				cin >> index; 
+				
+			        if (input == -1){
+					exit = true; 
+					return;
+				}
+				index = idfindcom
+				if (index == -1){
+					cout << "The input id cannot be found\n";
+				}
+				else{
+					input ok = true;
+				}
+			}
+			input ok = false; 
+			int choice;
+			while (not input ok){		
+				cout << "Which criteria are you going to use to edit?" << endl;
+				cout << "1. id\n";
+				cout << "2. name\n";
+				cout << "3. manufacturer\n";
+				cout << "4. price\n";
+				cout << "5. expiry date\n";
+				cout << "6. stock amount\n";
+				cout << "7. warning level\n";
+				cout << "8. stock warning\n";
+				cout << "9. out of stock\n";
+				cout << "10. shop id\n";
+				cout << "11. shop stock amount\n";
+				cout << "12. shop stock warning\n";
+				cout << "13. shop out of stock\n";
+				cout << "--------------------------------------------------------\n";
+				while (!input_ok){
+					cout << "Please enter a number: ";
+					cin >> buffer;
+					criterion = atoi(buffer.c_str());
+					if ((criterion < 1) || (criterion > 13)){
+						cout << "Please check your input and try again." << endl;
+					} 
+					else {
+						input_ok = 1;
+					}
+				}
+			}
+			switch (criterion){
+				case 1: { 
+					string value = getString();
+					int num = idfindcom(list, value);
+					if (num == -1){
+						cout << "There is contradiction to the entry ids.\n";
+					} 
+					else {
+						list[index].id = value;
+					}
+					break;
+				}
+				case 2: {
+					string value = getString();
+					vector<int> nums = idfindcom(list, value);
+					list[index].name = value; 
+					break; 
+				}
+				case 3: {
+					string value = getString();
+					vector<int> nums = idfindcom(list, value);
+					list[index].manufacturer = value; 
+					break; 
+				}
+				case 4: {
+					string value = getString();
+					vector<int> nums = idfindcom(list, value);
+					list[index].price = value;
+					break;
+				}
+				case 5: {
+					string value = getString();
+					vector<int> nums = idfindcom(list, value);
+					list[index].expiry_date = value; 
+					break; 
+				}
+				case 6: {
+					string value = getString();
+					vector<int> nums = idfindcom(list, value);
+					list[index].stock_amount = value; 
+					break; 
+				}
+				case 7: {
+					string value = getString();
+					vector<int> nums = idfindcom(list, value);
+					list[index].warning_level = value; 
+					break; 
+				}
+				case 8: {
+					string value = getString();
+					vector<int> nums = idfindcom(list, value);
+					list[index].stock_warning = value; 
+					break; 
+				}
+				case 9: {
+					string value = getString();
+					vector<int> nums = idfindcom(list, value);
+					list[index].out_of_stock = value; 
+					break; 
+				}
+				case 10: {
+					string value = getString();
+					vector<int> nums = idfindcom(list, value);
+					list[index].shop_id = value;
+					break; 
+				}
+				case 11: {
+					string value = getString();
+					vector<int> nums = idfindcom(list, value);
+					list[index].shop_stock_amount = value; 
+					break; 
+				}
+				case 12: {
+					string value = getString();
+					vector<int> nums = idfindcom(list, value);
+					list[index].shop_stock_warning = value; 
+					break; 
+				}
+				case 13: {
+					string value = getString();
+					vector<int> nums = idfindcom(list, value);
+					list[index].shop_out_of_stock = value; 
+					break; 
+				}
+			}
+		}
+	} 
 }
 
 // -----------------------------------
@@ -62,7 +196,21 @@ void replacec(vector<Commodity> &list, int index, Commodity target){
 // -----------------------------------
 // TODO: figure out the layout of the file to read
 void getUpdateFile(){
+	ifstream ifs;
+	string address;
 	
+	cout << "Regarding the file's format: Attributes are separated by semicolons and one leaves no new line at the end of the file,";
+	cout << "which is to be saved as a text file. The attributes come in order of id, name, manufacturer, unit price, expiry date (ddmmyyyy)";
+	cout << ", stock amount, stock warning level, shop id, shop stockpile amount. Each line is an entry in the inventory." << endl;
+	cout << "-------------------------------------------------------------------" << endl;
+	cout << "Enter the address of the inventory file with file extension: ";
+	cin >> address;
+	
+	ifs.open(address.c_str());
+	if (ifs.fail()){
+		cout << "Error occured opening the file." << endl;
+	}
+	ifs.close();
 }
 
 /*debug only
