3c4e24452425f19dda43b66b6584b47083804220
tommy-hku19 <47239468+tommy-hku19@users.noreply.github.com>
2019-04-26 16:27:37 +0800

    Update printinv.cpp
    finish the function void print_inv(vector<Commodity> list)

--- printinv.cpp
+++ printinv.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "printinv.h"
+#include <iostream>
 using namespace std;
 using namespace main_header;
 
@@ -11,7 +12,59 @@ using namespace main_header;
 // @return none
 // ---------------------------------
 void print_inv(vector<Commodity> list){
-
+	bool exit = 0;
+	string buffer;
+	bool input_ok = 0;
+	int criterion = 0;
+	vector<Commodity>::iterator position = list.begin();
+	while (not exit){
+		cout << "Commodity id: " << (*position).id << endl;
+		cout << "Commodity name: " << (*position).name << endl;
+		cout << "Commodity manufacturer: " << (*position).manufacturer << endl;
+		cout << "Commodity price: " << (*position).price << endl;
+		cout << "Commodity expiry date: " << (*position).expiry_date << endl;
+		cout << "Commodity stock amount: " << (*position).stock_amount << endl;
+		cout << "Commodity warning level: " << (*position).warning_level << endl;
+		cout << "Commodity stock warning: " << (*position).stock_warning << endl;
+		cout << "Commodity out of stock;: " << (*position).out_of_stock; << endl;
+		cout << "Commodity shop id: " << (*position).shop_id << endl;
+		cout << "Commodity shop amount: " << (*position).shop_amount << endl;
+		cout << "Commodity shop stock warning: " << (*position).shop_stock_warning << endl;
+		cout << "Commodity shop out of stock: " << (*position).shop_out_of_stock << endl;
+		cout << "------------------------------------------------------\n";
+		cout << "What action would you like to take?\n1. go to next item\n2. jump to an item\n3. return to the menu" << endl;
+	
+	while (!input_ok){
+		cout << "Please enter a number: ";
+		cin >> buffer;
+		criterion = atoi(buffer.c_str());
+		if ((criterion < 1) || (criterion > 3)){
+			cout << "Please check your input and try again." << endl;
+		} else {
+			input_ok = 1;
+		}
+	}
+	
+	switch(criterion){
+		case 1: {
+			position++;
+			if (position == list.end()) exit = true; //we are at the end of list 
+				break;
+		}
+		case 2: {
+			cin >> index
+			if ((index < 0 ) || (index >= (list.size()))
+				position = list.begin() + index;
+			break;
+		}
+		case 3: {
+			return exit = true; 
+			break; 
+		}
+		else{
+			cout << "There is an error in your input" << endl;
+		}
+	}
 }
 
 /*
